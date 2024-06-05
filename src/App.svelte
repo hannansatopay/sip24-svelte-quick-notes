8de58a83303b9def1d6e1ed19fd8ed485daf3f00
onkar69483 <onkar69483@gmail.com>
2024-06-05 23:34:04 +0530

    Part B Making the notes dynamic

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,74 @@
 <script>
   import {onMount} from 'svelte'
   
-  let title = '';
-  let note = '';
+  let pages = []
+  let currentPageIndex = 0
+  let title = ''
+  let note = ''
 
   onMount(()=>{
-    title = localStorage.getItem('title')
-    note = localStorage.getItem('note')
+    const savedPages = localStorage.getItem("pages")
+    if (savedPages) {
+      pages = JSON.parse(savedPages)
+      title = pages[currentPageIndex]
+      note = localStorage.getItem(title)
+    }else {
+      addPage()
+    }
   })
 
   function saveNote(){
-    console.log(title, note);
-    localStorage.setItem('title', title)
-    localStorage.setItem('note', note)
+    const storedPageName = pages[currentPageIndex]
+    if(storedPageName != title){
+      localStorage.removeItem(storedPageName)
+      pages[currentPageIndex] = title
+    }
+    localStorage.setItem(title, note)
+    localStorage.setItem("pages", JSON.stringify(pages))
+  }
+
+  function addPage() {
+    pages.push('New Page')
+    selectPage(pages.length ? pages.length-1 : 0)
+  }
+
+  function selectPage(index){
+    currentPageIndex = index
+    title = pages[currentPageIndex]
+    note = localStorage.getItem(title)
   }
 </script>
 
-<main class="p-4">
-  <input class="block w-full bg-gray-50 border border-gray-300 rounded-lg p-2.5" bind:value={title} type="text" placeholder="Title">
+<aside class="fixed top-0 left-0 z-40 w-60 h-screen">
+  <div class="bg-light-gray overflow-y-auto py-5 px-3 h-full border-gray-200" >
+    <ul class="space-y-2">
+      {#each pages as page, index}
+        <li>
+          <button on:click={()=>selectPage(index)} class="{index == currentPageIndex && 'bg-dark-gray'} py-2 px-3 text-gray-900 rounded-lg">{page}</button>
+        </li>
+      {/each}
+      <li class="text-center hover:text-gray-800">
+        <button class="font-medium" on:click={addPage}>+ Add Page</button>
+      </li>
+    </ul>
+  </div>
+</aside>
+
+<main class="p-4 ml-60 h-auto">
+  <div class="grid grid-cols-2 items-center mb-3">
+    <h1 class="text-3xl font-bold" contenteditable bind:textContent={title}></h1>
+    <button class="ml-auto bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm hover:bg-gray-900" on:click={saveNote}>Save</button>
+  </div>
+  <hr>
   <textarea class="mt-4 block w-full bg-gray-50 border border-gray-300 rounded-lg p-2.5" bind:value={note} name="note" placeholder="Take a note..."></textarea>
-  <button class="mt-4 bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm hover:bg-gray-900" on:click={saveNote}>Save</button>
 </main>
 
 <style>
+  .bg-light-gray {
+    background-color: rgb(235, 235, 235);
+  }
 
+  .bg-dark-gray {
+    background-color: rgb(204, 204, 204);
+  }
 </style>
