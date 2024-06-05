7810ac55f11ee21f6f284c2cd54ac60305602685
Hannan Satopay <sathannan@hotmail.com>
2024-06-05 15:11:42 +0530

    init

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+ 
+</script>
+
+<main>
+  Hello World
+</main>
+
+<style>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+const config = {
+  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
+
+  plugins: [require('flowbite/plugin')],
+
+  theme: {}
+};
+
+module.exports = config;
