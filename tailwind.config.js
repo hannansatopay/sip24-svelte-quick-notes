0a883262b9e9065b0e8c37ef24234b79394d963f
sahiln15 <sahilnavale2004@gmail.com>
2024-06-06 18:20:22 +0530

    sahil-navale

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
-
-  plugins: [require('flowbite/plugin')],
-
-  theme: {}
-};
-
-module.exports = config;
