3fce25be01545a26aba80730e254b92f3f1fa694
Divesh9 <diveshmaurya9819@gmail.com>
2024-06-06 21:03:22 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import {onMount} from 'svelte';
-
+ let pages=[];
+ let currentPageIndex = 0;
  let title='New Note';
  let note= 'Today is an Excellent Day!';
 
@@ -13,24 +14,43 @@
   localStorage.setItem('title',title);
   localStorage.setItem('note',note);
  }
+
+ function addPage() {
+  pages.push("New Page");
+  pages= pages;
+ }
 </script>
 
 <aside class="fixed top-0 left-0 z-40 w-60 h-screen">
-  <div class="bg-light-gray">
-    <ul>
+  <div class="bg-light-gray overflow-y-auto py-5 px-3 h-full border-r border-gray-200">
+    <ul class="space-y-2">
+      {#each pages as page, index}
       <li>
-        Hello
+        <button class="bg-dark-gray py-2 px-3 text-gray-900 rounded-lg">{page}</button>
       </li>
+      {/each}
+      <li class="text-center"><button on:click={addPage} class="font-medium">+ Add Page</button></li>
     </ul>
   </div>
 </aside>
 
-<main class="p-4">
-  <input class="block w-full bg-gray-50 border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={title} type='text' placeholder='Add title'>
+<main class="p-4 ml-60 h-auto">
+  <div class="grid grid-cols-2 items-center mb-3">
+    <h1 class="text-3xl font-bold" contenteditable bind:textContent={title}></h1>
+    <button class="ml-auto bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:click={saveNote}>Save</button>
+  </div>
+  <hr/>
   <textarea class="mt-3 block w-full bg-gray-50 border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={note}></textarea>
-  <button class="bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:click={saveNote}>Save</button>
 </main>
 
 <style>
+.bg-light-gray{
+  background: #fbfbfb;
+}
+
+.bg-dark-gray{
+  background: #EFEFEF;
+}
+
 
 </style>
