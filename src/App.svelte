ca002f72bbccdedb635a221bd8a41371234f4ed2
junaidbagwan146@gmail.com <user email"junaidbagwan146@gmail.com>
2024-06-06 22:43:41 +0530

    first half

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,24 @@
 <script>
- 
+  import {onMount} from 'svelte';
+ let title='hello';
+ let note='hii';
+
+onMount(() => {
+title=localStorage.getItem('title');
+note=localStorage.getItem('note');
+});
+function savenote(){
+  console.log(title,note);
+  localStorage.setItem('title',title);
+  localStorage.setItem('note',note);
+}
 </script>
 
-<main>
-  Hello World
+<main class="p-4">
+
+ <input class="block w-full bg-gray=50 border border-300 rounded-lg text-gray-900 p-2.5" bind:value={title} type='text'>
+ <textarea class="mt-3 block w-full bg-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5"bind:value={note}></textarea>
+ <button class="bg-gray-800 text-white px-5 py-2.5 rounded-lg font font-medium text-sm mt-3 hover:bg-gray-900"on:click={savenote}>save</button>
 </main>
 
 <style>
