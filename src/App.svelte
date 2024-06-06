30b44e6aff4cc4d9915d23a342c54ae57d4d7ec3
Priyanka Kumari <kumaripriyankasbg30@gmail.com>
2024-06-06 18:00:13 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,30 @@
 <script>
- 
+  import { onMount } from 'svelte';
+
+ let title = "New Note";
+ let note = "Today is an excelent day";
+
+ function saveNote(){
+  console.log(title,note);
+  localStorage.setItem('title',title);
+  localStorage.setItem('note',note);
+ }
+
+onMount(()=>{
+title = localStorage.getItem('title');
+note = localStorage.getItem('note');
+});
+
 </script>
 
-<main>
-  Hello World
+<main class="p-4">
+<input class ="block w-full bg-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={title} type = "text">
+
+<textarea class = "mt-3 block w-full bg-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={note}></textarea>
+
+<button class ="mt-3 bg-gray-800 text-white px-5 py-2.5 rounded-lg font-md text-sm hover:bg-gray-900" on:click={saveNote}>Save</button>
 </main>
 
 <style>
-
+ 
 </style>
