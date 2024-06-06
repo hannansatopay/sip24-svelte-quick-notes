f0922d38f9585550b087e8cf56c65b827504fe11
SamikshaBijwe <bijwesamiksha@gamil.com>
2024-06-06 18:42:43 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,25 @@
 <script>
- 
+import { onMount } from 'svelte';
+
+
+ let title = 'New Note';
+ let note = 'Today is an excellent day!';
+
+onMount(() => {
+  title = localStorage.getItem('title');
+  note = localStorage.getItem('note');
+})
+
+ function saveNote() {
+  localStorage.setItem('title', title);
+  localStorage.setItem('note', note);
+ }
 </script>
 
-<main>
-  Hello World
+<main class="p-4">
+  <input class="block w-full bg-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={title} type="text">
+  <textarea class="mt-3 block w-full bg-gray-50 border border-gray-300 rounded-lg text-gary-900 p-2.5" bind:value={note}></textarea>
+  <button class="bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-500" on:click={saveNote}>Save</button>
 </main>
 
 <style>
