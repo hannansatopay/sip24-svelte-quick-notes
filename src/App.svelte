19126e93e0c5bc3dbe6792481392bca70cbc2830
Anuraj Venkatpurwar <anurajvenkatpurwar@gmail.com>
2024-06-06 12:08:32 +0530

    implemented svelte- Anuraj-Venkatpurwar

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,65 @@
 <script>
- 
-</script>
+  import {onMount} from 'svelte';
 
-<main>
-  Hello World
+  let pages = [];
+  let currentPageIndex = 0;
+  let title = '';
+  let note = '';
+
+  onMount(() => {
+    const savedPages = localStorage.getItem("pages");
+    if(savedPages){
+      pages = JSON.parse(savedPages);
+      title = pages[currentPageIndex];
+      note = localStorage.getItem(title);
+    }else{
+      addPage();
+    }
+  });
+
+  function saveNote(){
+    pages[currentPageIndex] = title;
+    localStorage.setItem(title, note);
+    localStorage.setItem("pages", JSON.stringify(pages));
+  }
+  function addPage(){
+    pages.push("New Page");
+    selectPage(pages.length ? pages.length -1:0);
+  }
+
+  function selectPage(index){
+    currentPageIndex = index;
+    title = pages[currentPageIndex];
+    note = localStorage.getItem(title);
+  }
+</script>
+<aside class="fixed top-0 z-40 w-60 h-screen">
+  <div class="bg-light-gray overflow-y-auto py-5 px-3 h-full border-r border-gray-200 ">
+    <ul class="space-y-2">
+      {#each pages as page, index }
+      <li>
+        <button on:click={() => selectPage(index)} class="bg-dark-gray px-3 py-3 text-gray-900 rounded-lg" contenteditable bind:textContent={page}></button>
+      </li>
+      {/each}
+      <li class="text-center"><button on:click={addPage}>+ Add page</button></li>
+    </ul>
+  </div>
+</aside>
+
+<main class="p-4 ml-60 h-auto ">
+  <div class="grid grid-cols-2 items-center mb-3">
+    <h1 class="text-3xl font-bold" contenteditable>{title || "New Page"}</h1>
+    <button class="ml-auto bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:click={saveNote}>Save</button>
+  </div>
+  <hr>
+  <textarea class="block mt-3 w-full br-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={note}></textarea>
 </main>
 
 <style>
-
+ .bg-light-gray{
+  background: #FBFBFB;
+ }
+ .bg-dark-gray{
+  background: #EFEFEF;
+ }
 </style>
