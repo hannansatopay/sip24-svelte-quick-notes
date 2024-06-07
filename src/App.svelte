831812d79a5117df13378a6a855dc527f9f03c3e
aditya213037 <104627397+aditya213037@users.noreply.github.com>
2024-06-07 19:11:30 +0530

    Note app

--- src/App.svelte
+++ src/App.svelte
@@ -3,34 +3,63 @@
 <script>
   import { onMount } from "svelte";
 
-  
- let title = 'Note Title';
- let note = 'Write note here...';
+ let pages = [];
+ let currentPageIndex = 0;
+ let title = '';
+ let note = '';
  onMount(() => {
-     title = localStorage.getItem('title');
-     note = localStorage.getItem('note');
+     const savedpages = localStorage.getItem("pages")
+     if (savedpages) {
+      pages = JSON.parse(savedpages);
+      title = pages[currentPageIndex];
+      note = localStorage.getItem(title);
+      
+     } else {
+      addPage();
+     }
   })
 
 
  function savingNote(){
-  console.log(title);
-  console.log(note);
-  localStorage.setItem('title',title);
-  localStorage.setItem('note',note);
+  const savePageNote = pages[currentPageIndex];
+  if(savePageNote != title){
+    localStorage.removeItem(savePageNote);
+    pages[currentPageIndex]=title;
+  }
+  localStorage.setItem(title,note);
+  localStorage.setItem('pages',JSON.stringify(pages));
  }
+
+ function addPage(){
+  pages.push("New Page");
+  selectPage(pages.length ? pages.length-1 : 0)
+ }
+
+ function selectPage(index){
+  currentPageIndex = index;
+  title = pages[currentPageIndex];
+  note = localStorage.getItem(title);
+ }
+
 </script>
  <aside class="fixed h-screen w-60 top-0 left-0 z-40">
   <div class="bg-light-gray overflow-y-auto py-5 px-3 h-full border-r border-gray-200">
-    <ul>
+    <ul class="space-y-2">
+      {#each pages as page,index}
       <li>
-        <button class="bg-dark-gray border border-gray-300 py-2 px-3 text-gray-900 rounded-lg">{title || "New Page"}</button>
+        <button on:click={() => selectPage(index)} class="{index == currentPageIndex ? 'bg-dark-gray' : ''} border border-gray-300 py-2 px-3 text-gray-900 rounded-lg">{page}</button>
+      </li>
+      {/each}
+      
+      <li class="text-center">
+        <button class="font-medium" on:click={addPage}>+ Add Pages</button>
       </li>
     </ul>
   </div>
  </aside>
 <main class="p-4 ml-60 h-auto">
   <div class="grid grid-cols-2 item-center mb-3">
-    <h1 class="text-3xl font-bold">{title || "New Page"}</h1>
+    <h1 class="text-3xl font-bold p-2" contenteditable bind:textContent={title}>{title || "New Page"}</h1>
     <button class="ml-auto bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:click={savingNote}>Save</button>
   </div>
   <hr/>
