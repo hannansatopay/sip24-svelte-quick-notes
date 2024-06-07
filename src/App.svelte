7884a68c5cf7751674a2601948ee435281e4ec09
jasmitamukherjee <jasmitamukherjee4@gmail.com>
2024-06-07 10:54:49 +0530

    added styles

--- src/App.svelte
+++ src/App.svelte
@@ -7,10 +7,10 @@ console.log(title,note)
 }
 </script>
 
-<main>
- <input bind:value={title} type="text" placeholder="Add Title">
- <textarea bind:value={note}></textarea>
- <button on:click={saveNote}>Save</button>
+<main class="p-4">
+ <input class="block w=full bg-gray-50 border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={title} type="text" placeholder="Add your title here">
+ <textarea class="block w-full mt-3 bg-gray-50 border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={note}></textarea>
+ <button class="bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:click={saveNote}>Save</button>
 </main>
 
 <style>
