c7ebc5d846197022ae429785404062c27e577c47
jasmitamukherjee <jasmitamukherjee4@gmail.com>
2024-06-07 10:43:39 +0530

    Update App.svelte
    printing title and description on console

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,16 @@
 <script>
- 
+let title= '';
+let note ='';
+
+function saveNote(){
+console.log(title,note)
+}
 </script>
 
 <main>
-  Hello World
+ <input bind:value={title} type="text" placeholder="Add Title">
+ <textarea bind:value={note}></textarea>
+ <button on:click={saveNote}>Save</button>
 </main>
 
 <style>
