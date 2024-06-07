c4cdf487cb3e2c61c9765871eaff5bb2ea2a4193
Sejal Kalambhe <89303909+sejalk123@users.noreply.github.com>
2024-06-07 13:45:31 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,11 @@
 <script>
    
     import { onMount } from "svelte"; 
-
     let pages = [];
     let currentPageIndex = 0;
     let title = '';
     let note = '';
-
+    
     onMount(()=>{
       const savedPages = localStorage.getItem("pages");
       if(savedPages){
@@ -27,6 +26,10 @@
       localStorage.setItem(title,note);
       localStorage.setItem("pages",JSON.stringify(pages));
     }
+    function deleteNote(currentPageIndex){
+     const newPage = pages.filter(page=> page.id!=currentPageIndex.detail.id);
+     pages=newPage;
+    }
     function addPage(){
       pages.push("New Page");
       selectPage(pages.length ? pages.length-1 : 0);
@@ -36,6 +39,7 @@
       title = pages[currentPageIndex];
       note = localStorage.getItem[title];
     }
+   
 </script>
 
 <aside class="fixed top-0 left-0 w-60 h-screen">
@@ -55,12 +59,15 @@
    <div class="grid grid-cols-2 items-center mb-3">
       <h1 class="text-3xl font-bold" contenteditable bind:textContent={title}></h1>
       <button class="ml-auto bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:click={saveNote}>Save</button>
+      <hr>
+      <button class="ml-auto bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:click={deleteNote}>Delete</button>
    </div>
    <hr/>
    <!-- <input class="block w-full bg-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={title} type="text"  > -->
 
    <textarea class="mt-3 block w-full bg-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={note}></textarea>
-   <button class="bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:click={saveNote}>Save</button>
+   <!-- <button class="bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:click={saveNote}>Save</button> -->
+
 </main> 
 
 <style>
@@ -71,3 +78,4 @@
    background: #efefef;
 }
 </style>
+
