bfa8173cc1ac9518c73be5c21200c9e7c6f01ee9
Aayush <aayush.paigwar123@gmail.com>
2024-06-09 02:16:19 +0530

    Svelte Fundamentals (Part A)

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,42 @@
 <script>
- 
+  /*var
+  const 
+  let*/
+  import { onMount } from "svelte";
+  let title = "New Note";
+  let note = "Today is an awesome day!";
+
+  onMount(() => {
+    title = localStorage.getItem("title") || title;
+    note = localStorage.getItem("note") || note;
+  });
+  function savenote() {
+    console.log(title);
+    console.log(note);
+    localStorage.setItem("title", title);
+    localStorage.setItem("note", note);
+  }
 </script>
 
 <main>
-  Hello World
+  <!-- <h1>Hello World, I am {title}</h1> -->
+  <input
+    class="block w-full bg-gray50 border-gray-300 rounded-lg text-gray-900 p-2.5"
+    bind:value={title}
+    type="text"
+  />
+  <textarea
+    class="mt-3 block w-full bg-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5"
+    bind:value={note}
+  ></textarea>
+  <button
+    class=" bg-gray-800 text-white px-5 py-2.5 rounded-lg fount-medium text-sm mt-3 hover:bg-gray-900"
+    on:click={savenote}>Submit</button
+  >
 </main>
 
 <style>
-
+  /* h1 {
+    color: red;
+  } */
 </style>
