107df09c84045394a52dae0749b14280b5c5d616
PragnaThanam30 <pragna0830@gmail.com>
2024-06-08 20:42:15 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -15,7 +15,17 @@
  }
 </script>
 
-<main class="p-4">
+<aside class="fixed top-0 left-0 z-40 we-60 h-screen"> 
+  <div class="bg-light-gray overflow-y-auto py-5 px-3 h-full bordr-r border-gray-200">
+    <ul>
+      <li>
+        Hello
+      </li>
+    </ul>
+  </div>
+</aside>
+
+<main class="p-4 ml-60 h-auto">
  <input class="block w-full bg-gray-50 border border-gray-300 rounder=1g text-gray-900 p-2.5" bind:value={title} type='text'>
  <textarea class="mt-3 block w-full bg-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={note}></textarea>
  <button class="bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:clicl={saveNote}>Save</button>
@@ -23,5 +33,8 @@
 </main>
 
 <style>
+  .bg-light-gray {
+    background: #FBFBFB;
+  }
 
 </style> 
