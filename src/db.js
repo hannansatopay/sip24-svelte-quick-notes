6d3fbf202ff21a700ae039d9818b0133aefa0197
Priyanka Kumari <kumaripriyankasbg30@gmail.com>
2024-06-09 18:52:27 +0530

    Updated_files

---
+++ src/db.js
@@ -0,0 +1,14 @@
+
+import Dexie from 'dexie';
+
+export const db = new Dexie("notesApp");
+
+/*
+    database name notesApp with table name stickyNotes with fields primary key id,
+    field title and note created.
+*/
+db.version(1).stores(
+    {
+        stickyNotes:'++id, title, note'
+    }
+)
