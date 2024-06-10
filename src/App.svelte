dc58dd4360929af99ac694d4f328148b71cc6052
Shree Jaybhay <shreejaybhay26@gmail.com>
2024-06-10 08:56:33 +0530

    Updated

--- src/App.svelte
+++ src/App.svelte
@@ -81,9 +81,9 @@
           {#if currentPage === index}
             <button 
               on:click={() => deletePage(index)} 
-              class="w-8 h-8 ml-2 text-white transition duration-300 bg-red-600 rounded-full hover:bg-red-500"
+              class="flex items-center justify-center w-8 h-8 text-white transition duration-300 bg-red-600 rounded-full"
             >
-              X
+            <svg height='24' width='24' xmlns="http://www.w3.org/2000/svg" id="Delete" x="0" y="0" version="1.1" viewBox="0 0 29 29" xml:space="preserve"><path d="M10 3v3h9V3a1 1 0 0 0-1-1h-7a1 1 0 0 0-1 1z" fill="#ffffff" class="color000000 svgShape"></path><path d="M4 5v1h21V5a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1zM6 8l1.812 17.209A2 2 0 0 0 9.801 27H19.2a2 2 0 0 0 1.989-1.791L23 8H6zm4.577 16.997a.999.999 0 0 1-1.074-.92l-1-13a1 1 0 0 1 .92-1.074.989.989 0 0 1 1.074.92l1 13a1 1 0 0 1-.92 1.074zM15.5 24a1 1 0 0 1-2 0V11a1 1 0 0 1 2 0v13zm3.997.077a.999.999 0 1 1-1.994-.154l1-13a.985.985 0 0 1 1.074-.92 1 1 0 0 1 .92 1.074l-1 13z" fill="#ffffff" class="color000000 svgShape"></path></svg>
             </button>
           {/if}
         </div>
