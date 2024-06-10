09b09dba071cae5bb663bd138703e73554122af2
karimmirzaabdul <karimmirza2002@gmail.com>
2024-06-10 10:37:37 -0700

    Create karim svelte.svelte
    "Svelte" is a modern JavaScript framework used for building user interfaces. It distinguishes itself from other frameworks like React and Vue by shifting most of the work from runtime to compile time, resulting in highly optimized and efficient code.

---
+++ src/karim svelte.svelte
@@ -0,0 +1,19 @@
+<script>
+let title =" new note";
+let note = "today is an excellent day";
+
+function saveNote() {
+    console.log(title, note);
+}
+  </script>
+
+<main>
+  <input bind:value={title} type="text" placeholder="Add title">
+  <textarea bind:value={note}></textarea>
+  <button on:click={saveNote}>save</button>
+</main>
+
+<style>
+  
+
+</style>
