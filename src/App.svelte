c91df728d047b342c923375d7595ca74a8b7818e
tanuja1922 <139243492+tanuja1922@users.noreply.github.com>
2024-06-11 18:32:36 +0530

    Update App.svelte
    done with 2nd assignment

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,96 @@
 <script>
+  import {onMount} from 'svelte';
+  let pages= [];
+  let currentPageIndex=0;
+  let title = ' ';
+  let note = '';
+
+  onMount(() =>{
+      const savedPages = localStorage.getItem("pages");
+      if (savedPages){
+        pages = JSON.parse(savedPages);
+        title= pages[currentPageIndex];
+        note =localStorage.getItem(title);
+      }else{
+        addPage();
+
+      }
+      title= localStorage.getItem('title');
+      note= localStorage.getItem('note');
+    });
+  function saveNote(){
+    const storedPageName = pages[currentPageIndex];
+    if (storedPageName != title){
+      localStorage.removeItem(storedPageName);
+      pages[currentPageIndex] = title;
+    }
+    
+    localStorage.setItem(title, note);
+    localStorage.setItem("pages", JSON.stringify(pages));
+  
+   
+  }
+  function addPage(){
+    pages.push("New Page");
+    selectPage( pages.length ? pages.length - 1 :0);
+  }
+  function selectPage(index){
+    currentPageIndex = index;
+    title = pages[currentPageIndex];
+    note= localStorage.getItem(title);
+  }
+
+  //deletes page from local storage
+  function deletePage(index) {
+    const pageToDelete = pages[index];
+    if (confirm(`Are you sure you want to delete the page "${pageToDelete}"?`)) {
+      localStorage.removeItem(pageToDelete);
+      pages.splice(index, 1);
+      if (currentPageIndex >= pages.length) {
+        selectPage(pages.length - 1);
+      }
+      localStorage.setItem("pages", JSON.stringify(pages));
+    }
+  }
  
 </script>
+<aside class="fixed top-0 left-0 z-40 w-60 h-screen">
+  <div class="bg-light-gray overflow-y-auto py-5 px-3 h-full border-r border-gray-200">
+    <ul class="space-y-2">
+      
+        {#each pages as page, index}
+        <li>
+          <button on:click={()=>selectPage(index)} class="{index == currentPageIndex ? 'bg-dark-gray' : ''} py-2 px-3 text-gray-900 rounded-lg">{page}</button>
+          <button on:click={() => deletePage(index)} class="ml-2 text-red-600">Delete</button>
+        </li>
+        {/each}
+     
+  
+     <li class="text-center">
+      <button on:click={addPage} class="font-medium">+Add Page</button>
+      
+     </li>
+    </ul>
+  </div>
+</aside>
 
-<main>
-  Hello World
+<main class="p-4 ml-60 h-auto">
+  <div class="grid grid-cols-2  items-center mb-3">
+    <h1 class="text-3xl font-bold" contenteditable bind:textContent={title}></h1>
+    <button class=" ml-auto bg-gray-500 text-white px-4 py-2.5 mt-4 font-medium rounded-lg text-sm hover:bg-gray-800" on:click={saveNote} >Save</button>
+  </div>
+  <hr/>
+  
+  <textarea class="mt-3 block w-full bg-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={note}></textarea>
+  
 </main>
 
 <style>
+  .bg-light-gray{
+    background-color: #fbfbfb;
+  }
+  .bg-dark-gray{
+    background-color: #efefef ;
+  }
 
 </style>
