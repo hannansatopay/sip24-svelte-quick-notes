a07b6dd83cc18def8c997e6599c908595b983f73
yadavsk098 <yadavsk098@gmail.com>
2024-06-11 19:30:52 +0530

    completed svelte fundamental part B also

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,62 @@
 <script>
+  import { onMount } from "svelte";
  let name = "Sahil Yadav"
- let title = ""
- let note = ""
-
+ let title = "new Titlt"
+ let note = "new note"
+ let pages = []
+ let currentPageIndex = 0;
+ onMount(() => {
+  // title = localStorage.getItem('title')
+  // note = localStorage.getItem('note')
+  const savedPages = localStorage.getItem("pages");
+  if(savedPages){
+    pages = JSON.parse(savedPages) ; // convert back to string
+    title = pages[currentPageIndex] || "New page";
+    note = localStorage.getItem(title);
+  }
+  else{
+    addPage();
+  }
+ })
  function saveNote(){
-  localStorage.setItem("title",title);
-  localStorage.setItem("note",note);
+  // localStorage.setItem("title",title);
+  // localStorage.setItem("note",note);
+
+  const storedPagesName = pages[currentPageIndex];
+  if(storedPagesName != title){
+    // updation
+    pages[currentPageIndex] = title;
+  }
+
+  // pages[currentPageIndex] = title;
+  localStorage.setItem(title,note);
+  localStorage.setItem("pages",JSON.stringify(pages));
  }
+
+ function addPage(){
+  pages.push("New Page")
+  // pages = pages;
+  selectPage(pages.length ? pages.length -1 : 0)
+ }
+
+ function selectPage(index){
+  currentPageIndex = index;
+  title = pages[currentPageIndex];
+  note = localStorage.getItem(title);
+
+ }
+
 </script>
 
 <aside class="fixed top-0 left-0 z-40 w-60 h-screen">
-  <div class="bg-light-grey overflow-y-auto py-5 px-3 h-full border-r border-gray-200">
-    <ul>
+  <div class="bg-light-gray overflow-y-auto py-5 px-3 h-full border-r border-gray-200">
+    <ul class="space-y-2">
+      {#each pages as page,index}
       <li>
-        <button class="bg-dark-gray py-2 px-3 text-gray-900 rounded-lg">Page title</button>
-      </li>
+          <button on:click={()=>selectPage(index)} class="{index == currentPageIndex ? 'bg-dark-gray':''}  py-2 px-3 text-gray-900 rounded-lg">{page}</button>
+          </li>
+      {/each}
+      <li class="text-center"> <button on:click={addPage} class="font-medium">+ Add page</button> </li>
     </ul>
   </div>
 
@@ -24,15 +66,13 @@
  <!-- <h1>Hello my name is {name}</h1>  -->
   <!-- bind is two way linking   -->
 
-  <div>
-    <h1>Page Title</h1>
-<button class="bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:click={saveNote}>Save</button>
-
-    
-  </div>
-
-<input class="block w-full bg-gray-50 border border-gray-300 rounded-lg  text-gray-900 p-2.5" bind:value={title} type="text" placeholder="Add title">
-<textarea class="mt-3 block w-full bg-gray-50 border border-gray-300 rounded-lg  text-gray-900 p-2.5" bind:value={note} placeholder="add description"></textarea>
+<div class="grid grid-cols-2 items-center">
+  <h1 class="text-3xl font-bold" contenteditable bind:textContent={title}></h1>
+  <button class="ml-auto bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:click={saveNote}>Save</button>
+</div>
+<hr/>
+<!-- <input class="block w-full bg-gray-50 border border-gray-300 rounded-lg  text-gray-900 p-2.5" bind:value={title} type="text" placeholder="Add title"> -->
+<textarea class="mb-3 mt-3 block w-full bg-gray-50 border border-gray-300 rounded-lg  text-gray-900 p-2.5" bind:value={note} placeholder="add description"></textarea>
 </main>
 
 <style>
@@ -40,5 +80,11 @@
   color: rebeccapurple;
 } */
 
+.bg-light-gray{
+  background: #fbfbfb;
+}
+.bg-dark-gray{
+  background: #efefef;
+}
 
 </style>
