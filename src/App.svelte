e496c6a051eb05e1936dd7add689305098294ac5
Umed24 <109196694+Umed24@users.noreply.github.com>
2024-06-11 19:39:32 +0530

    basic layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,17 @@
 <script>
+  let title="New Note";
+  let note="Today is an excellent day!";
+
+  function saveNote(){
+    console.log(title,note);
+  }
  
 </script>
 
-<main>
-  Hello World
+<main class="p-4">
+  <input type="text" class="block w-full bg-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={title}>
+  <textarea  class="mt-3 block w-full bg-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={note}></textarea>
+  <button type="button" class="bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900"  on:click={saveNote}>Save</button>
 </main>
 
 <style>
