b85802b5a3a0b587ecb0af667d46cb9f13352de2
dhyanpatel18 <165238590+dhyanpatel18@users.noreply.github.com>
2024-06-12 09:03:57 +0530

    svelte part 1 implemented
    completed svelte fundamentals part 1

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,35 @@
 <script>
- 
+  import {onMount} from 'svelte';
+let title = 'New Note';
+let note = 'Today will be awesome';
+onMount(()=>{
+title = localStorage.getItem('title');
+note = localStorage.getItem('note');
+});
+function saveNote() {
+  console.log(title);
+  console.log(note);
+  localStorage.setItem('title',title);
+  localStorage.setItem('note',note);
+}
 </script>
 
-<main>
-  Hello World
+<main class="p-4">
+  <div >
+    <input class="block w-full bg-gray-50 border border-gray-300 runded-lg text-gray-900 p-2.5 " bind:value={title} type = "text" placeholder = "Enter the title" width = 600px>
+  </div>
+  <div>
+    <textarea class="block w-full mt-3 bg-gray-50 border border-gray-300 runded-lg text-gray-900 p-2.5" bind:value={note}></textarea>
+  </div>
+  <div class ="btn">
+    <button class="bg-blue-800 text-white px-2.5 py=2.5 rounded-lg font-medium text-sm mt-3 hover:bg-navy blue-700" on:click={saveNote}>SAVE</button>
+  </div>
 </main>
 
 <style>
-
+  .btn{
+  position: relative;
+  left:10px;
+  top: 10px;
+  }
 </style>
