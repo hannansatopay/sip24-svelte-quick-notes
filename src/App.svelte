812206bd5e6a43e169f75c806cf3f1b8a48496be
Gargi-2009 <gargi.randive@cumminscollege.edu.in>
2024-06-13 22:18:27 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,60 @@
 <script>
 import {onMount} from 'svelte';
 
-let Title='New Note';
-let note ='Today is an excellent day!';
+let pages=[];
+let currentPageIndex= 0;
+let Title='';
+let note ='';
 
 onMount(() => {
-Title=  localStorage.getItem('Title');
-note= localStorage.getItem('note');
+const savedPages = localStorage.getItem("pages");
+if(savedPages) {
+   pages = JSON.parse(savedPages);
+   Title = pages[currentPageIndex];
+   note = localStorage.getItem(Title);
+} else{
+   addPage();
+}
 });
 function saveNote() {
-   localStorage.setItem('Title',Title);
-   localStorage.setItem('note',note);
+   pages[currentPageIndex]=Title;
+   localStorage.setItem('Title',note);
+   localStorage.setItem("pages",JSON.stringify(pages));
+}
+function addPage() {
+   pages.push("New Page");
+  selectPage(pages.length ? pages.length-1:0);
+}
+function selectPage(index) {
+   Title = pages[currentPageIndex];
+   note=localStorage.getItem(Title);
+
 }
 </script> 
 
-<main class="p-4">
-  
- <input class="black w-full bg-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={Title} type="text">
+<aside class="fixed top-0 left-0 z-40 w-60 h-screen">
+   <div class="bg-light-gray overflow-y-auto py-5 px-3 h-full border-r border-gray-200">
+      <ul class="space-y-2">
+         {#each pages as page, index}
+         <li> 
+        
+           <button on:click={() =>selectPage(index)} class="bg-dark-gray py-2  px-3 text-gray-900 rounded-lg">{Title || "New Page"}</button>
+        
+         </li>
+        {/each}
+      <li class="text-center"><button on:click={addPage} class="font-medium">+Add Page</button></li>
+   </ul>
+   </div>
+</aside>
+
+<main class="p-4 ml-60 h-auto">
+  <div class="grid grid-cols-2 items-center mb-3">
+   <h1 class="text-3xl font-bold" contenteditable bind:textContent={Title}> </h1>
+   <button class="ml-auto bg-gray-800 text-white px-5 py2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:click={saveNote}> save</button>
+</div>
+ <hr/>
 <textarea class="mt-3 block w-full bg-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5"bind:value={note}></textarea>
-<button class="bg-gray-800 text-white px-5 py2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:click={saveNote}> save</button>
+
 </main>
  
 
@@ -26,5 +62,10 @@ function saveNote() {
 
 
 <style>
- 
+ .bg-light-gray{
+   background:#FBFBFB ;
+ }
+ .bg-dark-gray{
+   background: #EFEFEF;
+ }
 </style>
