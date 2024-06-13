09ea3e3832fab6e1a590b2d1ccc3685ae577bc6e
Gargi-2009 <gargi.randive@cumminscollege.edu.in>
2024-06-13 08:28:48 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,24 @@
 <script>
-let Title='New Note';
-let note ='Today is an excellent day';
+import {onMount} from 'svelte';
 
+let Title='New Note';
+let note ='Today is an excellent day!';
+
+onMount(() => {
+Title=  localStorage.getItem('Title');
+note= localStorage.getItem('note');
+});
 function saveNote() {
-   console.log(Title,note);
+   localStorage.setItem('Title',Title);
+   localStorage.setItem('note',note);
 }
 </script> 
 
-<main>
+<main class="p-4">
   
- <input bind:value={Title} type="text" placeholder="Add Title">
-<textarea bind:value={note}></textarea>
-<button on:click={saveNote}> save</button>
+ <input class="black w-full bg-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={Title} type="text">
+<textarea class="mt-3 block w-full bg-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5"bind:value={note}></textarea>
+<button class="bg-gray-800 text-white px-5 py2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:click={saveNote}> save</button>
 </main>
  
 
