360130d85ef50789acb5d2cde646a3f98ec4ecc0
SakshiVKadam2104 <151854591+SakshiVKadam2104@users.noreply.github.com>
2024-06-15 20:38:06 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,5 @@
 <script>
- import { onMount } from 'svelte';
- 
+import { onMount } from 'svelte';
  let pages= [];
  let currentPageIndex=0;
  let title=' ';
@@ -40,31 +39,28 @@ function selectPage(index){
   title=pages[currentPageIndex];
   note=localStorage.getItem(title);
 }
-
 </script>
+
 <aside class="fixed top-0 left-0 z-40 w-60 h-screen">
   <div class="bg-light-gray overflow-y-auto py-5 px-3 h-full border-r border-gray-200">
     <ul class="space-y-2">
       {#each pages as page,index}
       <li>
-       
         <button on:click={()=>selectPage(index)} class="{index == currentPageIndex ? 'bg-dark-gray' : ''} py-2 px-3 text-gray-900 rounded-lg">{page}</button>
-       
       </li>
       {/each}
       <li class="text-center"><button on:click={addPage} class="font-medium">+Add pages</button></li>
     </ul>
   </div>
 </aside>
+
 <main class="p-4 ml-60 h-auto">
   <div class="grid grid-cols-2 items-center mb-3">
     <h1 class="text-3xl font-bold" contenteditable bind:textContent={title}></h1>
     <button class=" ml-auto bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:click={saveNote}>save</button>
   </div>
 <hr/>
-
 <textarea class="mt-3 block w-full bg-gray-50 border border-gray-300 rounded-lg tex  t-gray-900 p-2.5" bind:value={note}></textarea>
-  
 </main>
 
 <style>
@@ -74,5 +70,4 @@ function selectPage(index){
 .bg-dark-gray{
   background: #efefef;
 }
-
 </style>
