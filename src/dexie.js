d4b4200040c95be9d5294d26252a0e490df597fd
Saiesh Gaonkar <129487105+Saiesh-Gaonkar@users.noreply.github.com>
2024-06-17 18:28:37 +0530

    Update dexie.js
    Removed  the comments written to track the  updates  and to know why was it written

--- src/dexie.js
+++ src/dexie.js
@@ -1,15 +1,12 @@
 import Dexie from 'dexie';
-
-// Extend Dexie to define the table schema
 class NotesDatabase extends Dexie {
   notes;
 
   constructor() {
     super('notesDatabase');
     this.version(1).stores({
-      notes: '++id, title, content' // Auto-incrementing primary key and indexes on title and content
+      notes: '++id, title, content' 
     });
-    // Reference to the 'notes' table
     this.notes = this.table('notes');
   }
 }
