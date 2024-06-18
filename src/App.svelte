ef5b16b0ca6b1689368be277de7ba80eb64d6a13
shubham karne <120401166+shubhamkarne11@users.noreply.github.com>
2024-06-18 19:40:33 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,8 @@
   onMount(() => {
     title = localStorage.getItem('title');
     note = localStorage.getitem('note');
+    if (title) title = title;
+    if (note) note = note;
   })
 
   function SaveNote() {
@@ -13,12 +15,21 @@
     localStorage.setItem('note', note);
 
   }
+
+  function deleteNote() {
+    localStorage.removeItem('title');
+    localStorage.removeItem('note');
+    title = '';
+    note = '';
+  }
 </script>
 
 <main class="p-4">
   <input class="block w-full bg-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={title} type='text' placeholder='Add Title'>
   <textarea class="mt-3 block w-full bg-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={note}></textarea>
   <button class="bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:click={SaveNote}>save</button>
+  <button
+  class="bg-red-600 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-red-700 ml-2" on:click={deleteNote}>Delete</button>
 </main>
 
 <style>
